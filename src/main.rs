2a37e8d44bf37d05d7cbf364189ee041f4290625
alSeiye5h <anasbelkhadir13@gmail.com>
2025-01-13 23:24:53 +0100

    chat

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use tokio {
-    io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt, BufReader},
     net::{TcpListener, TcpStream},
     sync::{mpsc, Mutex},
 };
@@ -48,7 +48,7 @@ async fn handle_client(
             yaeni t9der treceivi w tsendi fnfs lw9t yaeni concurrently b async
         */
         let (reader, mut writer) = socket.into_split();
-        let reader = BufReader::new(reader);
+        let mut reader = BufReader::new(reader);
 
         // unbounded channel mafihach limit d lcapacity, yaeni t9der tsendi 9esdma bghiti
         // dmessagat fiha wakha receiver mayreceivihoumch t9dr tb9a tseft fiha
@@ -57,29 +57,56 @@ async fn handle_client(
             let mut clients_lock = clients.lock().await;
             clients_lock.insert(addr.clone(), tx.clone());
         }
+        let adr = addr.clone();
+        let clients_for_read_task = clients.clone();
 
         // 
         let read_task = tokio::spawn(async move {
             let mut buffer = vec![0; 1024];
 
-            // loop {
-            //     match reader.read(&mut buffer).await {
-            //         Ok(0) => {
-            //             println!("{} disconnected", addr);
-            //         }
-            //         Ok(n) => {
-            //             let message = String::from_utf8_lossy(&buffer[..n]).to_string();
-            //             println!("{} >> {}", addr, message);
+            loop {
+                match reader.read(&mut buffer).await {
+                    Ok(0) => {
+                        println!("{} disconnected", addr);
+                        break;
+                    }
+                    Ok(n) => {
+                        let message = String::from_utf8_lossy(&buffer[..n]).to_string();
+                        println!("{} >> {}", addr, message);
 
-            //             // broadcaste lmessage lclients lokhrin
-            //             let clients_lock = clients.lock().await;
-            //             for (client_addr, tx) in clients_lock.iter() {
-            //                 if client_addr != &addr {
-
-            //                 }
-            //             }
-            //         }
-            //     }
+                        // broadcaste lmessage lclients lokhrin
+                        let clients_lock = clients_for_read_task.lock().await;
+                        for (client_addr, tx) in clients_lock.iter() {
+                            if client_addr != &addr {
+                                let _ = tx.send(format!("{} >> {}", addr, message));
+                            }
+                        }
+                    }
+                    Err(e) => {
+                        println!("error reading from socket: {}", e);
+                        break;
+                    }
+                }   
             }
-        })
+        });
+
+        let write_task = tokio::spawn(async move {
+            while let Some(msg) = rx.recv().await {
+                if writer.write_all(msg.as_bytes()).await.is_err() {
+                    break;
+                }
+            }
+        });
+
+        // wait for tasks to finish
+        let _ = tokio::try_join!(read_task, write_task);
+
+        // mli ideconnecta chi user heydo mn sharedstate
+        {
+            let mut clients_lock = clients.lock().await;
+            clients_lock.remove(&adr);
+        }
+
+        println!("Connection Closed: {}", adr);
+        Ok(())
 }
