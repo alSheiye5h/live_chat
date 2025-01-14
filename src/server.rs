b3765e0090f511e5ba4374e8c0c52a4a5e3d3396
alSeiye5h <anasbelkhadir13@gmail.com>
2025-01-14 13:00:47 +0100

    _

--- src/server.rs
+++ src/server.rs
@@ -35,4 +35,99 @@ async fn main() {
 
     // ncreaw listener (server)
     let ip_port = "127.0.0.1:2356";
+    let listener = TcpListener::bind(ip_port).await?;
+    println!("Chat server is Live on {}", ip_port);
+
+    loop {
+        // fch itconnecta chi wahed accepteh
+        let (socket, addr) = listener.accept().await;
+        let addr = addr.to_string();
+        // ----- add log here -----
+        println!("New Connection: {}", addr);
+
+        // cloni clients list
+        let clients = clients.clone();
+
+        // handli l connection
+        tokio::spawn(async move {
+            if let Err(e) = handle_client(socket, addr, clients).await {
+                eprintln!("Error: {}", e);
+            }
+        });
+    }
 }
+
+async fn handle_client(
+    socket: TcpStream,
+    addr: String,
+    clients: Arc<Mutex<HashMap<String, Tx>>>) 
+    -> Result<(), Box<(), dyn std::error::Error>> {
+         /* 
+            socket.into_split(), kat9sem socket wla tcpstream l two parts: read, write
+            bjojhom m implementying AsyncRead w AsyncWrite.
+            yaeni t9der treceivi w tsendi fnfs lw9t yaeni concurrently b async
+        */
+        let (reader, mut writer) = socket.into_split();
+        let mut reader = BufReader::new(reader); // wrappina reader f BufReader li chraht lfo9
+
+        // unbounded channel mafihach limit d lcapacity, yaeni t9der tsendi 9esdma bghiti
+        // dmessagat fiha wakha receiver mayreceivihoumch t9dr tb9a tseft fiha
+        let (tx, mut rx): (Tx, Rx) = mpsc::unbounded_channel();
+        {
+            let mut clients_lock = clients.lock().await;
+            clients_lock.insert(addr.clone(), tx.clone());
+        }
+
+        let addr2 = addr.clone(); // cloni l addr hit anhtaj n printiha flekher
+        let clients_for_read_task = clients.clone(); // hada hta houwa ghanhtaj npopi l address diali
+
+        let read_task = tokio::spawn(async move {
+            let mut buffer = vec![0, 1024];
+
+            loop {
+                match reader.read(&mut buffer).await { // reader: BufReader , rah m implemente trait dial AsyncReadExt dkchi elach read() method dial AsyncReadExt
+                    Ok(0) => {
+                        // ----- add log here -----
+                        println!("{} disconnected", addr);
+                        break;
+                    }
+                    Ok(n) => {
+                        let message = String::from_utf8_lossy(&buffer[..n]).to_string();
+                        println!("{} >> {}", addr, message);
+
+                        // broadcate lmessage l users li online
+                        let clients_lock = clients_for_read_task.lock().await;
+                        for (client_addr, tx) in clients_lock.iter() {
+                            let _ = tx.send(format!("{} >> {}", addr, message));
+                        }
+                        drop(clients_lock);
+                    }
+                    Err(e) => {
+                        // ----- add log here -----
+                        println!("error reading from socket: {}", e);
+                        break;
+                    }
+                }
+            }
+        });
+
+        let write_task = tokio::spawn(async move {
+            while let Some(msg) => rx.recv().await {
+                if writer.write_all(msg.as_bytes()).await.is_err() {
+                    break;
+                }
+            }
+        });
+
+        // wait for tasks to finish
+        let _ = tokio::try_join!(read_task, write_task);
+
+        // mli idecconecta chi user popih mn list
+        {
+            let mut clients_lock = clients.lock().await;
+            clients_lock.remove(&addr2);
+        }
+
+        println!("Connection Closed: {}", addr2);
+        Ok(())
+    }
